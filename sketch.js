68acad5d629d14d01c64cce20cbebb7c902ca383
Ern at home <ehymel@practicetally.com>
2024-06-23 16:15:58 -0500

    Work on falling

--- sketch.js
+++ sketch.js
@@ -8,11 +8,7 @@ function setup() {
     rows = height / w;
     grid = make2DArray(cols, rows);
 
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            grid[i][j] = 0;
-        }
-    }
+    grid[20][20] = 1;
 }
 
 function draw() {
@@ -28,12 +24,51 @@ function draw() {
         }
     }
 
+    let nextGrid = make2DArray(cols, rows);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let state = grid[i][j];
+            if (state === 1) {
+                let below = grid[i][j + 1];
+                let belowA, belowB;
+                let dir = random([-1, 1]);
+
+                if (i + dir > 0 && i - dir > 0 && i + dir < cols - 1 && i - dir < cols -1) {
+                    belowA = grid[i + dir][j + 1];
+                    belowB = grid[i - dir][j + 1];
+                }
+
+                if (below === 0) {
+                    nextGrid[i][j + 1] = 1;
+                } else if (belowA === 0) {
+                    grid[i + dir][j + 1] = 1;
+                } else if (belowB === 0) {
+                    grid[i - dir][j + 1] = 1;
+                } else {
+                    nextGrid[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    grid = nextGrid;
+}
+
+function mouseClicked() {
+    let col = floor(mouseX / w);
+    let row = floor(mouseY / w);
+    if (col >= 0 && col <= cols -1 && row >= 0 && row <= rows -1) {
+        grid[col][row] = 1;
+    }
 }
 
 function make2DArray(cols, rows) {
     let arr = new Array(cols);
     for (let i = 0; i < arr.length; i++) {
         arr[i] = new Array(rows);
+        for (let j = 0; j < arr[i].length; j++) {
+            arr[i][j] = 0;
+        }
     }
     return arr;
 }
