fe95b97bbeb7eafaa19d3d70db31e408f157d0c6
Ern in PA Office <ehymel@practicetally.com>
2024-06-24 16:05:06 -0500

    Add color

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let grid;
 let w = 5;
 let cols, rows;
+let hueValue = 0;
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(600, 800);
+    colorMode(HSB, 360, 255, 255);
     cols = width / w;
     rows = height / w;
     grid = make2DArray(cols, rows);
@@ -19,7 +21,7 @@ function fall() {
     let nextGrid = make2DArray(cols, rows);
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
-            if (1 === grid[i][j]) {
+            if (grid[i][j] > 0) {
                 let belowA, belowB;
                 let dir = random([-1, 1]);
 
@@ -31,13 +33,13 @@ function fall() {
                 }
 
                 if (0 === grid[i][j + 1]) {
-                    nextGrid[i][j + 1] = 1;
+                    nextGrid[i][j + 1] = grid[i][j];
                 } else if (belowA === 0) {
-                    nextGrid[i + dir][j + 1] = 1;
+                    nextGrid[i + dir][j + 1] = grid[i][j];
                 } else if (belowB === 0) {
-                    nextGrid[i - dir][j + 1] = 1;
+                    nextGrid[i - dir][j + 1] = grid[i][j];
                 } else {
-                    nextGrid[i][j] = 1;
+                    nextGrid[i][j] = grid[i][j];
                 }
             }
         }
@@ -52,8 +54,8 @@ function drawGrid() {
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             noStroke();
-            if (grid[i][j] === 1) {
-                fill(255);
+            if (grid[i][j] > 0) {
+                fill(grid[i][j], 255, 255);
                 let x = i * w;
                 let y = j * w;
                 square(x, y, w);
@@ -63,11 +65,23 @@ function drawGrid() {
 }
 
 function mouseDragged() {
-    let col = floor(mouseX / w);
-    let row = floor(mouseY / w);
-    if (col >= 0 && col <= cols -1 && row >= 0 && row <= rows -1) {
-        grid[col][row] = 1;
+    let mouseCol = floor(mouseX / w);
+    let mouseRow = floor(mouseY / w);
+
+    let matrix = 9;
+    let extent = floor(matrix / 2);
+    for (let i = -extent; i < extent; i++) {
+        for (let j = -extent; j < extent; j++) {
+            if (random(1) < 0.6) {
+                let col = mouseCol + i;
+                let row = mouseRow + j;
+                if (col >= 0 && col <= cols -1 && row >= 0 && row <= rows -1) {
+                    grid[col][row] = hueValue;
+                }
+            }
+        }
     }
+    hueValue = (hueValue + 0.5) % 360;
 }
 
 function make2DArray(cols, rows) {
