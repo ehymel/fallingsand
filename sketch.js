b473582807260bc54a36b61888d52b176b4f2ce4
E Hymel <ehymel@practicetally.com>
2024-06-25 19:07:33 -0500

    Add partition and handle landing on it

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ let grid;
 let w = 5;
 let cols, rows;
 let hueValue = 0;
+let partitionY;
 
 function setup() {
     createCanvas(600, 800);
@@ -9,6 +10,7 @@ function setup() {
     cols = width / w;
     rows = height / w;
     grid = make2DArray(cols, rows);
+    partitionY = floor(rows / 2);
 }
 
 function draw() {
@@ -22,9 +24,23 @@ function fall() {
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             if (grid[i][j] > 0) {
+                // if below is a partition (or the bottom), then we've landed
+                if (partitionY === j + 1 || rows === j + 1) {
+                    nextGrid[i][j] = grid[i][j];
+                    continue;
+                }
+
+                // if below is empty, drop down
+                if (0 === grid[i][j + 1]) {
+                    nextGrid[i][j + 1] = grid[i][j];
+                    continue;
+                }
+
+                // below is not empty, so try to fall to left or right (randomly) if possible
                 let belowA, belowB;
                 let dir = random([-1, 1]);
 
+                // make sure we are checking something still on the screen
                 if (i + dir >= 0 && i + dir <= cols - 1) {
                     belowA = grid[i + dir][j + 1];
                 }
@@ -32,13 +48,12 @@ function fall() {
                     belowB = grid[i - dir][j + 1];
                 }
 
-                if (0 === grid[i][j + 1]) {
-                    nextGrid[i][j + 1] = grid[i][j];
-                } else if (belowA === 0) {
+                if (belowA === 0) {
                     nextGrid[i + dir][j + 1] = grid[i][j];
                 } else if (belowB === 0) {
                     nextGrid[i - dir][j + 1] = grid[i][j];
                 } else {
+                    // can't fall to left or right, so we've landed
                     nextGrid[i][j] = grid[i][j];
                 }
             }
@@ -50,6 +65,10 @@ function fall() {
 
 function drawGrid() {
     background(0);
+
+    stroke(255);
+    strokeWeight(3);
+    line(0, partitionY * w, (cols - 1) * w, partitionY * w);
 
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
